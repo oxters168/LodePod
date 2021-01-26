0b25719745170f12631ff0c162ede1556b806c94
Oxters <oxters168@gmail.com>
2021-01-26 14:34:34 -0500

    Created a rudimentary input script to test out the pod physics. Added some random objects into the scene to act as obstacles. Updated UnityHelpers.

---
+++ Assets/Core/Scripts/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Rewired;
+
+public class PlayerInput : MonoBehaviour
+{
+    public int playerId = 0;
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(playerId);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pod = GetComponent<UnityHelpers.PodPhysics>();
+        pod.strafeStick = new Vector2(player.GetAxis("MoveHor"), player.GetAxis("MoveVer"));
+        pod.brake = player.GetAxis("Brake");
+        pod.rotate = player.GetAxis("Rotate");
+        pod.fly = player.GetAxis("Fly");
+        // Debug.Log(pod.rotateCW + ", " + pod.rotateCCW);
+    }
+}
